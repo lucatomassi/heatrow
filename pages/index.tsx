f3dc44dc9f883001655cdc15d473ba358b857abb
lucatomassi <luca@tomassi.ca>
2022-01-03 14:38:32 -0500

    genco web app

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,7 @@ const index: React.FC<indexProps> = ({}) => {
   return (
     <motion.div data-scroll-container ref={refScroll} initial='initial' animate='animate'>
       <Head>
-        <title>Heatrow &mdash; A Real Estate Company</title>
+        <title>Genco &mdash; Real Estate Management</title>
         <link rel='icon' href='/vercel.svg' />
       </Head>
       <header data-scroll-section className='home-header'>
@@ -90,8 +90,8 @@ const index: React.FC<indexProps> = ({}) => {
             <motion.img
               initial={{ x: -50, opacity: 0 }}
               animate={{ x: 0, opacity: 1, transition: { ...transition } }}
-              src='svg/heatrow-logo.svg'
-              alt='heatrow logo'
+              src='svg/groupe-genco.jpg'
+              alt='genco logo'
             />
             <motion.ul variants={navList} className='navigation__list'>
               <motion.li variants={navItem} className='navigation__item'>
@@ -116,23 +116,23 @@ const index: React.FC<indexProps> = ({}) => {
               initial={{ y: 50, opacity: 0 }}
               animate={{ y: 0, opacity: 1, transition: { delay: 0.2, ...transition } }}
               className='hero-h4'>
-              elegantly designed 3 <br />
-              bedroom apartments
+              Conveniently Located in Montreal <br />
+              Apartment Rentals to Fit Your Needs
             </motion.h4>
             <motion.h1
               initial={{ y: 50, opacity: 0 }}
               animate={{ y: 0, opacity: 1, transition: { delay: 0.6, ...transition } }}
               className='hero-h1'>
-              Maple
+              
             </motion.h1>
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, transition: { delay: 0.6, ...transition } }}
               className='home-hero__footer'>
-              <img src='svg/facebook-icon.svg' alt='facebook icon' />
-              <img src='svg/twitter-icon.svg' alt='twitter icon' />
-              <img src='svg/linkedIn-icon.svg' alt='linkedIn icon' />
-              <img src='svg/instagram-icon.svg' alt='instagram icon' />
+              <img src='svg/facebook.png' alt='facebook icon' />
+              <img src='svg/twitter.png' alt='twitter icon' />
+              <img src='svg/linkedin.png' alt='linkedIn icon' />
+              <img src='svg/instagram.png' alt='instagram icon' />
             </motion.div>
           </div>
         </div>
@@ -140,14 +140,14 @@ const index: React.FC<indexProps> = ({}) => {
           <motion.img
             initial={{ opacity: 0, scale: 1.4, x: 400 }}
             animate={{ opacity: 1, scale: 1, x: 0, transition: { delay: 1.1, ...transition } }}
-            src='webp/header-bg.webp'
-            alt='maple house'
+            src='webp/montrealskyline.webp'
+            alt='montreal skyline'
           />
           <motion.div
             initial={{ y: 200, opacity: 0 }}
             animate={{ y: 0, opacity: 1, transition: { delay: 1.8, ...transition } }}
             className='hero-unit'>
-            <h2 className='hero-unit__h2'>444</h2>
+            <h2 className='hero-unit__h2'>122</h2>
             <p className='hero-unit__p'>TOTAL UNITS</p>
           </motion.div>
         </div>
@@ -156,72 +156,57 @@ const index: React.FC<indexProps> = ({}) => {
         <section data-scroll-section id='sectionFeatures' className='section-features'>
           <div className='feature-container'>
             <div className='feature'>
-              <img src='svg/faces-icon.svg' alt='face icon' />
+              <img src='svg/design.svg' alt='face icon' />
               <h2 className='heading-2'>
-                Familiar <br />
-                Faces
+                Quality Design 
+                & Materials
               </h2>
               <p className='paragraph'>
-                Our facial recognition system cross-references faces attempting to gain access into
-                the Heatrow Estate with the register database of residents. We value your safety so
-                no one gets in unless you say so.
+              We go to great lengths designing apartments to provide our residents with the highest level of quality and comfort.
               </p>
             </div>
             <div className='feature'>
-              <img src='svg/eye-icon.svg' alt='eye icon' />
+              <img src='svg/service.svg' alt='eye icon' />
               <h2 className='heading-2'>
-                Eye in the <br />
-                Sky
+                Reliable
+                Service
               </h2>
               <p className='paragraph'>
-                Our infrared (IR) outdoor bullet cameras are immune to whatever or lighting, and
-                come in different resolutions and lenses. Sit back anywhere in the world and record
-                everything going on in and around your home with crisp clear audio.
+              At Genco, our team places great value on reliability, authenticity, and customer service.
               </p>
             </div>
             <div className='feature'>
-              <img src='svg/anpr-icon.svg' alt='face icon' />
-              <h2 className='heading-2'>ANPR</h2>
+              <img src='svg/location.svg' alt='face icon' />
+              <h2 className='heading-2'>Convenient Locations</h2>
               <p className='paragraph'>
-                Our Automatic Number Plate Recognition system scans every plate trying to gain
-                access into the Heatrow estate and cross references with our database of registered
-                plates. Our advances algorithms can decode even blurry and dark images.
+              We believe that where you live should compliment how you live, and we strive to make sure our units provide exactly that.
               </p>
             </div>
             <div className='feature'>
-              <img src='svg/smart-icon.svg' alt='smart icon' />
+              <img src='svg/choice.svg' alt='smart icon' />
               <h2 className='heading-2'>
-                Smart <br />
-                Savings
+                Ample Choices
               </h2>
               <p className='paragraph'>
-                Save up to 50% more energy with our smart energy-consumption monitoring. Enjoy smart
-                system in your home that help you dim lights, and even turn off home appliances when
-                not in use. Even water leakages are detected in real-time if you ever leave the tap
-                running.
+              Designed for both convenience and comfort, we have all types of units at many different price ranges.
               </p>
             </div>
             <div className='feature'>
-              <img src='svg/wifi-icon.svg' alt='wifi icon' />
+              <img src='svg/extra.svg' alt='wifi icon' />
               <h2 className='heading-2'>
-                Fibre to <br />
-                the Home
+                Added Services
               </h2>
               <p className='paragraph'>
-                Our finer-optic cables are setup around the estate to deliver lightening fast
-                internet whenever you need it at the comfort of your home. We present to you the
-                future of broadband.
+              Hot Water, Electricity, Internet, Appliances, and more. We have many added services which can be included to make life easier for you. 
               </p>
             </div>
             <div className='feature'>
-              <img src='svg/door-icon.svg' alt='door icon' />
+              <img src='svg/team.svg' alt='door icon' />
               <h2 className='heading-2'>
-                The Walls <br />
-                Have Ears
+                Devoted Team
               </h2>
               <p className='paragraph'>
-                Lock the doors, open the windows control the lights, and much more without lifting a
-                finger, The tongue really is powerful. Go on, Speak, your home us listening.
+              We place great value on your security and wellbeing, and you can take comfort in knowing that you’re well looked after by our loyal team.
               </p>
             </div>
           </div>
@@ -229,36 +214,37 @@ const index: React.FC<indexProps> = ({}) => {
         <section data-scroll-section id='sectionGallery' className='section-gallery'>
           <div className='gallery-container'>
             <h2 className='heading-2'>
-              Live in the <br /> Future
+              Our Properties <br /> Your Home
             </h2>
             <div className='gallery'>
               <div className='gallery__left'>
                 <div className='gallery__left--top'>
-                  <img src='webp/villa-bg.webp' alt='villa image' />
-                  <h4 className='gallery-caption'>15 villa</h4>
+                  <img src='webp/LF.png' alt='lf image' />
+                  <h4 className='gallery-caption'>10875 Av. Louis-Frechette</h4>
                 </div>
                 <div className='gallery__left--bottom'>
                   <h3 className='gallery-h3'>
-                    Comrfort <br /> is a choice
+                    Browse <br /> our properties
                   </h3>
-                  <p className='gallery-p'>and we offer 4 of those</p>
+                  <p className='gallery-p'>Find Your Home</p>
                 </div>
               </div>
               <div className='gallery__right'>
                 <div className='gallery__right--top'>
                   <div className='gallery__right--top-left'>
-                    <img src='webp/oak-bg.webp' alt='oak image' />
-                    <h4 className='gallery-caption'>155 oak</h4>
+                    <img src='webp/HAMEL.png' alt='hamel image' />
+                    <h4 className='gallery-caption'>1495 Boul. Henri-Bourassa E</h4>
                   </div>
                   <div className='gallery__right--top-right'>
-                    <img src='webp/oakville-bg.webp' alt='oakville image' />
-                    <h4 className='gallery-caption'>30 oakville</h4>
+                    <img src='webp/BORDEAUpng.png' alt='bordeau image' />
+                    <h4 className='gallery-caption'>8135 Rue De Bordeaux</h4>
                   </div>
                 </div>
                 <div className='gallery__right--bottom'>
-                  <img src='webp/maple.webp' alt='maple image' />
-                  <h4 className='gallery-caption'>234 maple</h4>
+                  <img src='webp/CHARLEROI.png' alt='charleroi image' />
+                  <h4 className='gallery-caption'>4800 Rue De Charleroi</h4>
                 </div>
+                
               </div>
             </div>
           </div>
@@ -272,36 +258,34 @@ const index: React.FC<indexProps> = ({}) => {
             </div>
             <div className='feedback-right'>
               <div className='feedback-box'>
-                <img src='svg/quoute-icon.svg' alt='quoute icon' />
+                <img src='svg/quote.png' alt='quoute icon' />
                 <p className='paragraph'>
-                  I love the fact that there is a linking road in Mabushi opposite blue Cabana which
-                  is really amazing. I love the road network and the structure is impressive.
+                Lorem ipsum dolor sit amet, consectetur adipiscing 
+                elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Lorem dolor sed viverra ipsum nunc aliquet bibendum. Ullamcorper malesuada proin libero nunc consequat
                 </p>
                 <h5 className='heading-5'>Mr & Mrs Bamidele</h5>
               </div>
               <div className='feedback-box'>
-                <img src='svg/quoute-icon.svg' alt='quoute icon' />
+                <img src='svg/quote.png' alt='quoute icon' />
                 <p className='paragraph'>
-                  So far so good, services really fantastic and the area is fantastic. The quality
-                  of work is fantastic and I look forward to a more beneficial relationship.
-                  Cosgrove equals fantastic for me.
+                Gravida neque convallis a cras. Facilisis sed odio morbi quis commodo odio aenean. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. A scelerisque purus semper eget. Diam maecenas ultricies mi eget mauris pharetra. 
                 </p>
                 <h5 className='heading-5'>Ojo Daniel</h5>
               </div>
               <div className='feedback-box'>
-                <img src='svg/quoute-icon.svg' alt='quoute icon' />
+                <img src='svg/quote.png' alt='quoute icon' />
                 <p className='paragraph'>
-                  Not bad I visited your site and was a bit confused about the villa and detached
-                  spacing but thanks to the officer in charge I got clarification. I am actually an
-                  ambassador for Cosgrove because I believe in the brand.
+                Vel turpis nunc eget lorem dolor sed viverra ipsum nunc. 
+                Tincidunt lobortis feugiat vivamus at augue eget arcu dictum.
+                 Integer malesuada nunc vel risus commodo viverra maecenas.
                 </p>
                 <h5 className='heading-5'>Mr & Mrs Joseph</h5>
               </div>
               <div className='feedback-box'>
-                <img src='svg/quoute-icon.svg' alt='quoute icon' />
+                <img src='svg/quote.png' alt='quoute icon' />
                 <p className='paragraph'>
-                  I love the fact that there is a linking road in Mabushi opposite blue Cabana which
-                  is really amazing. I love the road network and the structure is impressive.
+                Ligula ullamcorper malesuada proin libero nunc. At ultrices mi tempus imperdiet. Et malesuada fames ac turpis. 
                 </p>
                 <h5 className='heading-5'>Mr & Mrs Danladi</h5>
               </div>
@@ -335,41 +319,41 @@ const index: React.FC<indexProps> = ({}) => {
                 viewBox='0 0 152 35.674'>
                 <g transform='translate(-66 -71.663)'>
                   <g transform='translate(5929.179 -1324.144)'>
-                    <path d='M-5839.528,1412.126l-23.651,19.29v-35.609h23.651Z' fill='#FABE7D' />
+                    <path d='M-5839.528,1412.126l-23.651,19.29v-35.609h23.651Z' fill='#7dbefa' />
                     <path
                       d='M-5789.339,1415.3l23.651-19.29v35.609h-23.651Z'
                       transform='translate(-50.491 -0.14)'
-                      fill='#FABE7D'
+                      fill='#7dbefa'
                     />
                   </g>
                   <text
                     transform='translate(132 99)'
-                    fill='#FABE7D'
+                    fill='#7dbefa'
                     fontSize='24'
                     fontFamily='HelveticaNeue-Medium, Helvetica Neue'
                     fontWeight='500'
                     letterSpacing='-0.053em'>
                     <tspan x='0' y='0'>
-                      Heatrow
+                      Genco
                     </tspan>
                   </text>
                 </g>
               </svg>
               <p className='footer-p'>
-                +234813 900 1052
+                +1 514 744 4400
                 <br />
                 <br />
-                +234802 830 1153
+                +1 514 266 2927
               </p>
             </div>
             <div className='footer__bottom--box'>
               <p className='footer-p'>
-                1st Floor, Mujjab El Yakub Building, Danladi Street, <br />
-                Central Business District Abuja. FCT Nigeria.
+                230 Boulevard Henri-Bourassa E, Suite 100 <br />
+                Montreal, Quebec, H3L 1B8, CA.
               </p>
             </div>
           </div>
-          <p className='footer-copyright'>© Copyright 2021 Heatrow Estate</p>
+          <p className='footer-copyright'>© Copyright 2022 Groupe Genco Immobiliare</p>
         </div>
       </footer>
     </motion.div>
